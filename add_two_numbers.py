97c0013ea4e2f2bc953f659b26117a1234b64a15
Penina Wanjiru <penina.wanjiru@andela.com>
2019-10-01 22:08:38 +0300

    Refactor add two nums

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -12,7 +12,6 @@ class Solution:
         start_node_l1 = l1.next
         start_node_l2 = l2.next
         new_values = []
-        # if value1 and value2:
         new_value = value1 + value2
         if new_value > 9:
             str_value = str(new_value)
@@ -27,17 +26,12 @@ class Solution:
 
             new_val1 = current_node_l1.val
             new_val2 = current_node_l2.val
-            # if new_val1 and new_val2:
             new_val = new_val1 + new_val2 + remainder
             remainder = 0
             if new_val > 9:
                 str_value = str(new_val)
                 new_val = int(str_value[-1])
                 remainder = int(str_value[0])
-            # elif new_val1:
-            #     new_val = new_val1
-            # elif new_val2:
-            #     new_val = new_val2
             new_values.append(new_val)
             current_node_l1 = current_node_l1.next
             current_node_l2 = current_node_l2.next
@@ -47,24 +41,3 @@ class Solution:
                 node.next = new_nodes[index + 1]
 
         return new_nodes[0]
-
-
-
-
-
-
-# listNode1 = ListNode(2)
-# listNode2 = ListNode(4)
-# listNode3 = ListNode(3)
-
-# listNode2.next = ListNode3
-# listNode1.next = listNode2
-
-# listNodea = ListNode(5)
-# listNodeb = ListNode(6)
-# listNodec = ListNode(4)
-
-# listNodeb.next = listNodec
-# listNodea.next = listNodeb
-
-# addTwoNumbers(listNodea, listNode1)
