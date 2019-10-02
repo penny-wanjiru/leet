9b6d12921b5fa99b7da48dfa03b5010feaf1b9a8
Penina Wanjiru <penina.wanjiru@andela.com>
2019-10-03 00:11:02 +0300

    Add check for linked list length & break out of loop

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -5,20 +5,29 @@ class ListNode:
         self.next = None
 
 class Solution:
+    def linked_list_length(self, node):
+        "Return the length of the linked list starting at node."
+        length = 0
+        while node:
+            length += 1
+            node = node.next
+        return length
+
     def addTwoNumbers(self, l1, l2):
         remainder = 0
-        start_node_l1 = l1.next
-        start_node_l2 = l2.next
+        next_node_l1 = l1.next
+        next_node_l2 = l2.next
         new_values = []
         new_value = l1.val + l2.val
         if new_value > 9:
             rem, new_value = str(new_value)
             remainder = int(rem)
         new_values.append(int(new_value))
-        current_node_l1 = start_node_l1
-        current_node_l2 = start_node_l2
-
+        current_node_l1 = next_node_l1
+        current_node_l2 = next_node_l2
+        length = 0
         while current_node_l1 and current_node_l2 is not None:
+            length += 1
             new_val = current_node_l1.val + current_node_l2.val + remainder
             remainder = 0
             if new_val > 9:
@@ -27,11 +36,17 @@ class Solution:
             new_values.append(int(new_val))
             current_node_l1 = current_node_l1.next
             current_node_l2 = current_node_l2.next
+            if self.linked_list_length(l1)== len(new_values):
+                break
+        else:
+            new_values.append(remainder)
         new_nodes = [ListNode(val) for val in new_values]
         for index, node in enumerate(new_nodes):
             if index + 1 < len(new_nodes):
                 node.next = new_nodes[index + 1]
         return new_nodes[0]
+
+
 
 listNode1 = ListNode(2)
 listNode2 = ListNode(4)
