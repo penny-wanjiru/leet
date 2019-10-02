13111b768bde976c2138e1c0171e37ef17b8aa88
Penina Wanjiru <penina.wanjiru@andela.com>
2019-10-02 22:26:22 +0300

    Fix failing new value iteration

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -12,8 +12,8 @@ class Solution:
         new_values = []
         new_value = l1.val + l2.val
         if new_value > 9:
-            remainder, new_value = str(new_value)
-            remainder = int(remainder)
+            rem, new_value = str(new_value)
+            remainder = int(rem)
         new_values.append(int(new_value))
         current_node_l1 = start_node_l1
         current_node_l2 = start_node_l2
@@ -22,9 +22,9 @@ class Solution:
             new_val = current_node_l1.val + current_node_l2.val + remainder
             remainder = 0
             if new_val > 9:
-                remainder, new_value = str(new_val)
-                remainder = int(remainder)
-            new_values.append(int(new_value))
+                rem, new_val = str(new_val)
+                remainder = int(rem)
+            new_values.append(int(new_val))
             current_node_l1 = current_node_l1.next
             current_node_l2 = current_node_l2.next
         new_nodes = [ListNode(val) for val in new_values]
@@ -32,7 +32,7 @@ class Solution:
             if index + 1 < len(new_nodes):
                 node.next = new_nodes[index + 1]
         return new_nodes[0]
-        
+
 listNode1 = ListNode(2)
 listNode2 = ListNode(4)
 listNode3 = ListNode(3)
